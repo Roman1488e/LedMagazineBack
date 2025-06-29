95a99c95c64b662f62d99b7829f4ab24cd862730
Roman1488e <vertushkinroman@gmail.com>
2025-06-29 18:12:37 +0500

    migrations

---
+++ LedMagazineBack/Migrations/20250628082720_4.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LedMagazineBack.Migrations
+{
+    /// <inheritdoc />
+    public partial class _4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VideoUrl",
+                table: "Articles",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VideoUrl",
+                table: "Articles",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
